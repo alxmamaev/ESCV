b50830441abc7af0eccfdc1f4c5830105a577b1a
AZbang <azbango5@gmail.com>
2016-09-26 17:55:56 +0300

    create 2 templates

--- server/app/views.py
+++ server/app/views.py
@@ -5,3 +5,11 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.jade")
+
+@app.route("/people")
+def people():
+    return render_template("people.jade")
+
+@app.route("/rooms")
+def rooms():
+    return render_template("rooms.jade")
