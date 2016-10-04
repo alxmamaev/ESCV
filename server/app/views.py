3b705a1b07262491a37226ed0e0729a98e3c4872
AZbang <azbango5@gmail.com>
2016-10-04 12:19:19 +0300

    new templates for room and user :alien:

--- server/app/views.py
+++ server/app/views.py
@@ -2,14 +2,43 @@
 from app import app
 from flask import render_template
 
+# Типа база данных =D
+peopleData = [
+	{
+		"id": 0,
+		"img": "../static/img/nevskiy!.jpg",
+		"name": "Alx Nevskiy!",
+		"description": "Do you speak Engish?"
+	}
+]
+roomsData = [
+	{
+		"id": 0,
+		"img": "../static/img/stolovka.jpg",
+		"label": "The best dining room in the world!",
+		"description": "Burgers... With Pasta?!"
+	}
+]
+# Конец типа базы данных
+
 @app.route("/")
 def index():
     return render_template("index.jade")
 
 @app.route("/people")
 def people():
-    return render_template("people.jade")
+    return render_template("people.jade", people = peopleData)
 
 @app.route("/rooms")
 def rooms():
-    return render_template("rooms.jade")
+    return render_template("rooms.jade", rooms = roomsData)
+
+@app.route("/people")
+@app.route("/people/<int:man>")
+def man(man):
+    return render_template("man.jade", man = peopleData[man])
+
+@app.route("/rooms")
+@app.route("/rooms/<int:room>")
+def room(room):
+    return render_template("room.jade", room = roomsData[room])
