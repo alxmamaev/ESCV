5d85f2ef868e996c0e81598e132fe00ce87baa9b
AZbang <azbango5@gmail.com>
2016-10-25 14:41:41 +0300

    create visit_list

--- server/app/views.py
+++ server/app/views.py
@@ -3,18 +3,28 @@ from app import app
 from flask import render_template
 from app import base
 
+debug_user_list = [{
+	"room": "Столовка",
+	"time": "14:13",
+	"date": "25.10.2016"
+}]
+debug_room_list = [{
+	"user": "Андрей Жевлаков",
+	"time": "14:13",
+	"date": "25.10.2016"
+}]
+
 @app.route("/")
 def index():
     return render_template("index.jade")
 
-@app.route("/user/<int:user_id>")
-def user(user_id):
-    return render_template("user.jade", user = base.user_info(user_id))
-
-
 @app.route("/users")
 def users():
     return render_template("users.jade", users = base.users_list())
+
+@app.route("/user/<int:user_id>")
+def user(user_id):
+    return render_template("user.jade", user = base.user_info(user_id), visit_list = debug_user_list)
 
 @app.route("/rooms")
 def rooms():
@@ -22,4 +32,4 @@ def rooms():
 
 @app.route("/rooms/<int:room_id>")
 def room(room_id):
-    return render_template("room.jade", room = base.room_info(room_id))
+    return render_template("room.jade", room = base.room_info(room_id), visit_list = debug_room_list)
