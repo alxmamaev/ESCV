0c4675ce61586b83e687acfd5682f83980795ab0
AlexanderMamaev <alxmamaev@mail.ru>
2016-12-07 18:32:14 +0500

    Create proxy

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 from app import app
+from werkzeug.contrib.fixers import ProxyFix
 
-if __name__ == "__main__": app.run(debug = True)
+if __name__ == "__main__": 
+	app.wsgi_app = ProxyFix(app.wsgi_app)
+	app.run(debug = True)
