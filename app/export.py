cc08e28ca31d5be85523f4e48ac73fb5ef6d40d2
AlexanderMamaev <alxmamaev@mail.ru>
2016-12-07 20:01:52 +0500

    Fixed export crash on linux

--- app/export.py
+++ app/export.py
@@ -4,9 +4,9 @@ import pandas as pd
 from app import base
 
 def to_csv(visits):
-    file_name = "export\\%s.csv" % hex(random.randint(0, 1000000))
+    file_name = "export/%s.csv" % hex(random.randint(0, 1000000))
     visits = pd.DataFrame(visits)
     if visits.get("user_id") is not None: visits.pop("user_id")
     if visits.get("room_id") is not None: visits.pop("room_id")
-    visits.to_csv("app\\"+file_name, index=False)
+    visits.to_csv("app/"+file_name, index=False)
     return file_name
