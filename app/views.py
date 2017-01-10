fd4928ca1764e094b328fe518f49d0b395b2ddad
Alexander Mamaev <alxmamaev@ya.ru>
2017-01-10 18:15:39 +0300

    Views refactoring

--- app/views.py
+++ app/views.py
@@ -25,7 +25,8 @@ def user(user_id):
     if start_date is None: start_date = cur_date
     if end_date is None: end_date = cur_date
 
-    return render_template("user.jade", user = base.user_info(user_id),
+    return render_template("user.jade",
+                           user = base.user_info(user_id),
                            visit_list = base.user_visits(user_id, start_date, end_date),
                            cur_date = cur_date,
                            start_date = start_date,
@@ -57,7 +58,8 @@ def room(room_id):
     if start_date is None: start_date = cur_date
     if end_date is None: end_date = cur_date
 
-    return render_template("room.jade", room = base.room_info(room_id),
+    return render_template("room.jade",
+                           room = base.room_info(room_id),
                            visit_list = base.room_visits(room_id, start_date, end_date),
                            cur_date = cur_date,
                            start_date = start_date,
@@ -88,11 +90,10 @@ def new_visit():
         room_id = request.args.get("room_id")
         user_id = request.args.get("user_id")
 
-    if (rfid_id is None and user_id is None) or room_id is None:
-        return "Opps",404
+    if (rfid_id is None and user_id is None) or room_id is None: return "Opps",404
 
-    if rfid_id is not None: req = base.new_visit(rfid_id = rfid_id,room_id = room_id)
-    else: req = base.new_visit(user_id = user_id,room_id = room_id)
+    if rfid_id is not None: req = base.new_visit(rfid_id = rfid_id, room_id = room_id)
+    else: req = base.new_visit(user_id = user_id, room_id = room_id)
 
     app.logger.info(req)
 
