63ce8598560437ade8a9d3c95d05e07f66ddd0cd
AZbang <azbango5@gmail.com>
2017-01-31 12:48:39 +0300

    fix calendar range

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,14 +1,13 @@
 $(function() {
-	console.log($('#start_date_input').val());
 	new Calendar({
 		element: $('.daterange--double'),
 		earliest_date: '2000-01-1',
 		latest_date: moment(),
-		start_date: moment(),
+		start_date: '2000-01-1',
 		end_date: moment(),
 		callback: function() {
-			$('#start_date_input').val(moment(this.start_date).format('YY-MM-DD'));
-			$('#start_date_input').val(moment(this.end_date).format('YY-MM-DD'));
+			$('#start_date_input').val(moment(this.start_date).format('YYYY-MM-DD'));
+			$('#end_date_input').val(moment(this.end_date).format('YYYY-MM-DD'));
 
 			$('form').submit();
 		}
